38344297e2093fefe551b7dc1257a42197411308
Norips <raphdruon@gmail.com>
2016-09-18 20:51:50 +0200

    Correct url

--- projetCom/urls.py
+++ projetCom/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 urlpatterns = [
-    url(r'^form/', include('Formulaire.urls')),
+    url(r'^', include('Formulaire.urls')),
     url(r'^admin/', admin.site.urls),
 ]
