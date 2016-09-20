aad57097e659307c040bf693484ca44f85084f85
Norips <raphdruon@gmail.com>
2016-09-20 14:21:37 +0200

    Password and field and local jquery

--- Formulaire/forms.py
+++ Formulaire/forms.py
@@ -4,6 +4,9 @@ from .models import Form,FieldForm
 class PollForm(forms.ModelForm):
     class Meta:
         model = Form
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
         fields = ('title','password')
 
 class QuestionForm(forms.ModelForm):
